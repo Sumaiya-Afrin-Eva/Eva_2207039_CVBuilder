c9fa46aea536296f0dbce94c3ca6920b89243087
Eva <sumaiyaafrineva@Sumaiyas-MacBook-Air.local>
2025-11-14 23:35:49 +0600

    Adding ScrollPane in HomeController

--- src/main/java/com/example/cv_builder/HomeController.java
+++ src/main/java/com/example/cv_builder/HomeController.java
@@ -4,20 +4,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.Parent;
 
 public class HomeController {
-    private void go(ActionEvent e, String fxml) {
-        try{
-            Parent root = FXMLLoader.load(getClass().getResource(fxml));
-            Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root, 900, 700));
-            stage.show();
-        }catch(Exception ex){ ex.printStackTrace(); }
+
+    @FXML
+    void openCreateCV(ActionEvent event) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("create.fxml"));
+
+        ScrollPane scroll = new ScrollPane();
+        scroll.setContent(root);
+        scroll.setFitToWidth(true);
+
+        Scene createScene = new Scene(scroll, 900, 700);
+
+        createScene.getStylesheets().add(
+                getClass().getResource("create.css").toExternalForm());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(createScene);
+        stage.show();
     }
-
-    @FXML void openCreateCV(ActionEvent e){ go(e, "create.fxml"); }
 }
-
