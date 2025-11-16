4fa13d724e719c68176f5d19ad1130ac291819eb
Eva <sumaiyaafrineva@Sumaiyas-MacBook-Air.local>
2025-11-16 21:17:58 +0600

    Adding Controller Class on create.fxml

--- src/main/java/com/example/cv_builder/CreateController.java
+++ src/main/java/com/example/cv_builder/CreateController.java
@@ -1,4 +1,27 @@
 package com.example.cv_builder;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class CreateController {
+
+    @FXML
+    void openGenerateCV(ActionEvent event) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("generate.fxml"));
+
+        Scene scene = new Scene(root, 900, 700);
+        scene.getStylesheets().add(
+                getClass().getResource("generate.css").toExternalForm()
+        );
+
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
